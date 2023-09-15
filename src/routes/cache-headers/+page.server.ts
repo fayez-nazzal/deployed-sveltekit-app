329c3cd00f04528889233dc5ef12009c2f42152c
Fayez Nazzal <soulldev@gmail.com>
2023-09-15 16:23:25 +0300

    Enable csr for cache headers route

--- src/routes/cache-headers/+page.server.ts
+++ src/routes/cache-headers/+page.server.ts
@@ -1,6 +1,7 @@
-import { delayedFetchComments } from '$lib/server/utils';
 import type { ServerLoad } from '@sveltejs/kit';
 import dayjs from 'dayjs';
+
+export const csr = true;
 
 export const load: ServerLoad = async ({ fetch, setHeaders }) => {
 	const time = dayjs().format('HH:mm:ss');

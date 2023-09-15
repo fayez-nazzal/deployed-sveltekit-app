e9634f38cc1baa5fb9cd444dc828a1c707741828
Fayez Nazzal <soulldev@gmail.com>
2023-09-15 16:07:30 +0300

    Enable csr for page with streaming

--- src/routes/ssr-streaming/+page.server.ts
+++ src/routes/ssr-streaming/+page.server.ts
@@ -2,6 +2,7 @@ import { delayedFetchComments } from '$lib/server/utils';
 import type { ServerLoad } from '@sveltejs/kit';
 
 export const ssr = true;
+export const csr = true; // required for streaming
 
 export const load: ServerLoad = async ({ fetch }) => {
 	return {

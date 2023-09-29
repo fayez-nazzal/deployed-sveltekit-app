64c54e25d65fca7470c2b5ca9a33501318f6a22c
Fayez Nazzal <soulldev@gmail.com>
2023-09-29 14:12:43 +0300

    Prerender all entries

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,12 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			enabled: true,
+			crawl: true,
+			entries: ['*']
+		}
 	}
 };
 
